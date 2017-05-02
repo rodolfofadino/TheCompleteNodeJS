5813d23b7aebf3d614e935ba7e9d43a485add2b2
RodolfoFadino <rodolfo.fadino@gmail.com>
2017-05-02 08:51:40 -0300

    scope

---
+++ scope.js
@@ -0,0 +1,11 @@
+var age = 24;
+function localFunction(){
+    //console.log(age);
+    // var name = 'name';
+    // console.log(name);
+    var age = 0;
+    age = 0;
+}
+
+localFunction();
+console.log(age);
